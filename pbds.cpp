71837e679d4f2135972cf3eb0ebfb98f22f8abe7
Devveloper122 <158509966+Devveloper122@users.noreply.github.com>
2024-12-23 19:21:29 +0530

    2nd version

--- pbds.cpp
+++ pbds.cpp
@@ -3,7 +3,7 @@
 #include<ext/pb_ds/tree_policy.hpp>
 
 using namespace __gnu_pbds;
-typedef tree<long long, null_type, less<long long>, rb_tree_tag, tree_order_statistics_node_update> pbds; 
+typedef tree<long long, null_type, less_equal<long long>, rb_tree_tag, tree_order_statistics_node_update> pbds; 
 
 // 1). Basic functions:
 
